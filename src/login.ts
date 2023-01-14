8e96b1827fe8d65412c7e51cafc84a4eeb4ca730
Denys Kharkov <kharkov.denys@lll.kpi.ua>
2023-01-14 01:26:14 +0100

    Finding an error on the server

--- src/login.ts
+++ src/login.ts
@@ -4,13 +4,16 @@ import { addToken, deleteToken } from "./db";
 export default async function login(ctx: CommandContext<Context>): Promise<void> {
     try {
         const arg = ctx.match.trim().split(' ');
+        console.log('1');
         try {
             await ctx.deleteMessage();
         } catch {
             console.log("Помилка видалення повідомлення");
         }
+        console.log('2');
         if (!ctx.from || arg.length !== 2) { ctx.reply("Сталася якась помилка"); return; }
         await deleteToken(ctx.from.id.toString());
+        console.log('3');
         const details: { [key: string]: string } = {
             'username': arg[0],
             'password': arg[1],
@@ -28,12 +31,15 @@ export default async function login(ctx: CommandContext<Context>): Promise<void>
             },
             body: formBody.join("&")
         });
+        console.log('4');
         const token = data.headers.get('set-cookie')?.match(/token=([^;]*);/)?.[1];
         if (!token) { ctx.reply("Токен повернувся пустим"); return; }
+        console.log('5');
         const added = await addToken(ctx.from.id.toString(), token);
         if (!added) { ctx.reply("Сталась помилка при додаванні"); return; }
         ctx.reply("Автентифікація пройшла успішно");
     } catch (e) {
         ctx.reply("Сталася якась помилка");
+        console.log(e);
     }
 }
