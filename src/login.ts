43991f3cce6d906a8f4c7f7e340d0c46854670a3
Denys Kharkov <kharkov.denys@lll.kpi.ua>
2023-01-14 01:04:52 +0100

    Catch all login errors

--- src/login.ts
+++ src/login.ts
@@ -2,29 +2,33 @@ import { CommandContext, Context } from "grammy";
 import { addToken, deleteToken } from "./db";
 
 export default async function login(ctx: CommandContext<Context>): Promise<void> {
-    const arg = ctx.match.trim().split(' ');
-    ctx.deleteMessage().catch((e) => console.log(e));
-    if (!ctx.from || arg.length !== 2) { ctx.reply("Сталася якась помилка"); return; }
-    await deleteToken(ctx.from.id.toString());
-    const details: { [key: string]: string } = {
-        'username': arg[0],
-        'password': arg[1],
-        'grant_type': 'password'
-    };
-    const formBody = [];
-    for (const property in details) {
-        const encodedKey = encodeURIComponent(property);
-        const encodedValue = encodeURIComponent(details[property]);
-        formBody.push(encodedKey + "=" + encodedValue);
+    try {
+        const arg = ctx.match.trim().split(' ');
+        await ctx.deleteMessage();
+        if (!ctx.from || arg.length !== 2) { ctx.reply("Сталася якась помилка"); return; }
+        await deleteToken(ctx.from.id.toString());
+        const details: { [key: string]: string } = {
+            'username': arg[0],
+            'password': arg[1],
+            'grant_type': 'password'
+        };
+        const formBody = [];
+        for (const property in details) {
+            const encodedKey = encodeURIComponent(property);
+            const encodedValue = encodeURIComponent(details[property]);
+            formBody.push(encodedKey + "=" + encodedValue);
+        }
+        const data = await fetch("https://api.campus.kpi.ua/oauth/token", {
+            method: 'POST', headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            },
+            body: formBody.join("&")
+        });
+        const token = data.headers.get('set-cookie')?.match(/token=([^;]*);/)?.[1];
+        if (!token) { ctx.reply("Сталася якась помилка"); return; }
+        if (!await addToken(ctx.from.id.toString(), token)) { ctx.reply("Сталася якась помилка"); return; }
+        ctx.reply("Автентифікація пройшла успішно");
+    } catch (e) {
+        ctx.reply("Сталася якась помилка");
     }
-    const data = await fetch("https://api.campus.kpi.ua/oauth/token", {
-        method: 'POST', headers: {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-        },
-        body: formBody.join("&")
-    });
-    const token = data.headers.get('set-cookie')?.match(/token=([^;]*);/)?.[1];
-    if (!token) { ctx.reply("Сталася якась помилка"); return; }
-    if (!await addToken(ctx.from.id.toString(), token)) { ctx.reply("Сталася якась помилка"); return; }
-    ctx.reply("Автентифікація пройшла успішно");
 }
